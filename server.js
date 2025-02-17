2209eeb534b718f627e582b8f3ed508988382919
cnart002 <128757308+cnart002@users.noreply.github.com>
2025-02-16 19:04:13 -0600

    Updated app message for DevOps showcase

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker!');
+  res.send('Hello from Caleb Nartey! This Node.js app is running on AWS EKS, deployed with Kubernetes and GitHub Actions. DevOps in action!"');
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
